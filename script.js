4ea6385fbd50d62693ec45b3385c95f65ed33ee5
Antisocialglitch <okoroifeanyi39@gmail.com>
2024-11-19 23:29:53 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let bread = 450
+const tariff = 4.5
+
+let middleFinger = "Rage"
+let fingersUp = 1
+let isRage = true 
+let rageMode = { strength:1500, pmove: "assault", isFacts:true };
+let person = { name:'Alex', age: 25 };
+let bodyCount = [2,4,5,8]
+let heHe = fingersUp + streng
+console. log (heHe) ;
+    console.log(bodyCount)
+ console. log(isRage)
